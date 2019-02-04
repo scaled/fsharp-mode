846d766a3f039efbac4e20c09af7a216fed1b13f
Michael Bayne <mdb@samskivert.com>
2019-02-04 14:37:45 -0800

    Rudimentary, but helpful auto-indent support.

--- src/scala/FSharpMode.scala
+++ src/scala/FSharpMode.scala
@@ -5,7 +5,7 @@
 package scaled.fsharp
 
 import scaled._
-import scaled.code.{Commenter, BlockIndenter}
+import scaled.code.{Commenter, Indenter}
 import scaled.grammar.GrammarCodeMode
 
 @Major(name="fsharp",
@@ -16,16 +16,29 @@ class FSharpMode (env :Env) extends GrammarCodeMode(env) {
 
   override def langScope = "source.fsharp"
 
-  override protected def createIndenter = new BlockIndenter(config, Std.seq(
-    // bump extends/implements in two indentation levels
-    BlockIndenter.adjustIndentWhenMatchStart(Matcher.regexp("(extends|implements)\\b"), 2),
-    // align changed method calls under their dot
-    new BlockIndenter.AlignUnderDotRule(),
-    // handle indenting switch statements properly
-    new BlockIndenter.SwitchRule(),
-    // handle continued statements, with some special sauce for : after case
-    new BlockIndenter.CLikeContStmtRule()
-  ))
+  override protected def createIndenter = new Indenter(config) {
+    import Indenter._
+    override def apply (info :Info) = if (info.row == 0) 0 else {
+      val prev = info.buffer.line(info.row-1)
+      val prevFirst = prev.firstNonWS
+      // if prev line ends with '=' or '->', indent one step
+      if (endsWith(prev, equalsM) || endsWith(prev, arrowM)) prevFirst + indentWidth
+      // indent match cases under 'function' by one step
+      else if (endsWith(prev, functionM)) prevFirst + indentWidth
+      // indent 'match x when' cases under 'match'
+      else if (endsWith(prev, withM) && prev.indexOf(matchM) >= 0) prev.indexOf(matchM)
+      // if previous line is blank, leave indentation alone
+      else if (prevFirst == prev.length) info.first
+      // otherwise indent to same level as previous line
+      else prevFirst
+    }
+
+    private val equalsM = Matcher.exact("=")
+    private val arrowM = Matcher.exact("->")
+    private val matchM = Matcher.regexp("\\bmatch\\b")
+    private val withM = Matcher.regexp("\\bwith\\b")
+    private val functionM = Matcher.regexp("\\bfunction\\b")
+  }
 
   override val commenter = new Commenter() {
     override def linePrefix  = "//"
